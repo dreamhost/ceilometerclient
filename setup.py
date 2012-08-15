e5e13cb0363d419b0d5a7cbf2474d82786d3b5b4
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-08-15 15:02:27 -0400

    clean up; add trove classifiers; add dependency on requests
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

--- setup.py
+++ setup.py
@@ -5,15 +5,33 @@ import setuptools
 setuptools.setup(
     name='ceilometerclient',
     version='0.1',
+
     description='Client for ceilometer metering API',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 #'Programming Language :: Python :: 3',
+                 #'Programming Language :: Python :: 3.2',
+                 'Intended Audience :: Developers',
+                 'Environment :: Web Environment',
+                 ],
+
     author='Doug Hellmann',
     author_email='doug.hellmann@dreamhost.com',
+
     url='https://github.com/dreamhost/ceilometerclient',
+
     packages=setuptools.find_packages(exclude=['bin']),
     include_package_data=True,
+
     test_suite='nose.collector',
+
     scripts=[],
-    py_modules=[],
-    entry_points=textwrap.dedent("""
-    """),
+
+    install_requires=['requests'],
+
+    zip_safe=False,
     )
