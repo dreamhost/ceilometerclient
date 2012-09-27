a4648ac1029a0da45d24dad8c5ceaff3656a0935
Steven Berler <steven.berler@dreamhost.com>
2012-09-27 11:59:39 -0700

    adds volumes and images to csv dump
    Volumes and images are also dumped in the csv tool.
    Adds columns for the resource type, name, display_name, and size.
    These columns are populated from the resource_metadata, except
    volumes will query the API for their max size.

--- tools/dump_csv.py
+++ tools/dump_csv.py
@@ -8,22 +8,37 @@ import sys
 import ceilometerclient
 
 
-FIELD_NAMES = ['project_id', 'resource_id', 'instance_flavor',
-               'first_seen', 'last_seen', 'duration']
+RESOURCE_FIELDS = ['project_id', 'resource_id', 'name', 'display_name',
+                   'type', 'instance_flavor', 'first_seen', 'last_seen',
+                   'duration', 'size']
 
-def dump_instances(ceilometer, dumper):
+def dump_resources(ceilometer, dumper):
     for project_id in ceilometer.get_projects():
         if project_id is None:
             continue  # for some reason we get None sometimes
 
         for resource in ceilometer.get_resources(project_id=project_id):
             resource_id = resource['resource_id']
+            metadata = resource['metadata']
             meters = [item.get('counter_name') for item in resource['meter']]
 
             for meter in meters:
+                type_ = None
+                flavor = None
+                size = metadata.get('size')
                 if meter.startswith('instance:'):
+                    type_ = 'instance'
                     flavor = meter.partition(':')[-1]
+                elif meter == 'volume_size':
+                    type_ = 'volume'
+                    size = ceilometer.get_resource_volume_max(
+                        resource_id=resource_id,
+                        meter=meter,
+                        )
+                elif meter == 'image_size':
+                    type_ = 'image'
 
+                if type_ is not None:
                     duration_info = ceilometer.get_resource_duration_info(
                         resource_id=resource_id,
                         meter=meter,
@@ -32,10 +47,14 @@ def dump_instances(ceilometer, dumper):
                     dumper.writerow(dict(
                         project_id=project_id,
                         resource_id=resource_id,
+                        name=metadata.get('name'),
+                        display_name=metadata.get('display_name'),
+                        type=type_,
                         instance_flavor=flavor,
-                        first_seen=duration_info['start_timestamp'],
-                        last_seen=duration_info['end_timestamp'],
-                        duration=duration_info['duration'],
+                        first_seen=duration_info.get('start_timestamp'),
+                        last_seen=duration_info.get('end_timestamp'),
+                        duration=duration_info.get('duration'),
+                        size=size,
                         ))
 
 def main():
@@ -49,9 +68,9 @@ def main():
 
     ceilometer = ceilometerclient.Client(args.clienturl)
     with open(args.filename, 'wb') as csvfile:
-        dumper = csv.DictWriter(csvfile, FIELD_NAMES)
+        dumper = csv.DictWriter(csvfile, RESOURCE_FIELDS)
         dumper.writeheader()
-        dump_instances(ceilometer, dumper)
+        dump_resources(ceilometer, dumper)
 
 
 if __name__ == '__main__':
